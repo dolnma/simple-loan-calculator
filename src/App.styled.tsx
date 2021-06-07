02f718bdc68c84ea303ba1da8db7606451df0b74
Marek Dolnicek <marekdolnicek@gmail.com>
2021-06-07 13:21:43 +0200

    added sliders and selects with default data from api

--- src/App.styled.tsx
+++ src/App.styled.tsx
@@ -3,3 +3,10 @@ import styled from 'styled-components'
 export const StyledApp = styled.div`
     background: #ccc;
 `;
+
+export const StyledSliders = styled.div`
+    padding: 50px;
+`;
+export const StyledSlidersSlider = styled.div`
+    margin-top: 30px;
+`;
